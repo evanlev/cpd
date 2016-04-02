a1a1df356b2ca1b36702e5794fc3052a9047058c
evanlev <evanlevine138e@gmail.com>
2016-04-01 21:23:21 -0700

    Added files via upload

---
+++ publishResults.m
@@ -0,0 +1,10 @@
+clear, close all;
+
+publish('demoCPDAll.m', 'html');
+
+%publish('demoCPDSetup.m', 'html');
+%publish('demoCPD1.m', 'html');
+%publish('demoCPD2.m', 'html');
+%publish('demoCPD3.m', 'html');
+%publish('demoCPD4.m', 'html');
+
